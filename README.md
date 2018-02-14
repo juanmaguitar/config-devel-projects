73e27e79d68397fa5fe7ac74406ea647194d5fa0
JuanMa Garrido <juanma.garrido@gmail.com>
2018-02-14 21:02:48 +0100

    documentation

---
+++ README.md
@@ -0,0 +1,7 @@
+# Devel projects configuration
+
+This script will automatically create `.babelrc` & `.eslintrc` w/ proper settings and will install proper dependencies
+
+```
+sh -c "$(curl -fsSL https://rawgit.com/juanmaguitar/f8f09d9eda71d8bd77b88c8bfac32cda/raw/e9c769734951577fa82593eaed0482208351d4ab/install.sh)"
+```
---
+++ install.sh
@@ -0,0 +1,5 @@
+npm i -D babel-preset-es2015 babel-preset-stage-2 babel-preset-react
+npm i -D babel-eslint eslint-config-standard eslint-plugin-import eslint-plugin-node eslint-plugin-promise eslint-plugin-standard prettier-eslint
+
+curl -fsSL https://rawgit.com/juanmaguitar/config-devel-projects/master/.babelrc > .babelrc
+curl -fsSL https://rawgit.com/juanmaguitar/config-devel-projects/master/.eslintrc > .eslintrc
