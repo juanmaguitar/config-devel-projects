79eabc07fa31e38253b5e05c87192e3b8a140f53
JuanMa Garrido <juanma.garrido@gmail.com>
2018-02-15 20:52:24 +0100

    fixed issue

--- eslint-standard-prettier/install.sh
+++ eslint-standard-prettier/install.sh
@@ -4,6 +4,6 @@ curl -fsSL $GITHUB_RAW_URL".babelrc" > .babelrc
 curl -fsSL $GITHUB_RAW_URL".eslintrc" > .eslintrc
 
 npm i lodash request request-promise
-GITHUB_RAW_URL=$GITHUB_RAW_URL node -e '$(curl -fsSL $GITHUB_RAW_URL"mergePackage.js")'
+GITHUB_RAW_URL=$GITHUB_RAW_URL node -e "$(curl -fsSL $GITHUB_RAW_URL\"mergePackage.js\")"
 cat package.json
 npm i
