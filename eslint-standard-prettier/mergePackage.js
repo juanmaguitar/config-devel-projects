90159b73af69ce3adce19abd5250cff114e6b2fd
JuanMa Garrido <juanma.garrido@gmail.com>
2018-02-15 19:34:01 +0100

    cleaned config paths

--- eslint-standard-prettier/mergePackage.js
+++ eslint-standard-prettier/mergePackage.js
@@ -3,22 +3,21 @@ const _ = require('lodash');
 const path = require('path')
 const rp = require('request-promise')
 
-const urlConfigPackage = 'https://raw.githubusercontent.com/juanmaguitar/config-devel-projects/master/eslint-standard-prettier/package.json'
-const pathCurrentPackage =  path.join(__dirname,'package.json')
+const PACKAGE_NAME = 'package.json'
+const urlRemote = 'https://raw.githubusercontent.com/juanmaguitar/config-devel-projects/master/eslint-standard-prettier/'
 
-console.log(urlConfigPackage)
-console.log(pathCurrentPackage)
+const urlRemoteConfigPackage =  path.join(urlRemote, PACKAGE_NAME)
+const pathCurrentPackage =  path.join(__dirname,PACKAGE_NAME)
 
 (async function () {
+  const configPackage = await rp(urlRemoteConfigPackage)
+  const currentPackage = await fs.readFile(pathCurrentPackage, 'utf-8')
 
-  const configPackage = await rp(urlConfigPackage)
-  
-  const currentPackage = fs.readFileSync(pathCurrentPackage, 'utf-8')
   const mergedPackage = _.merge(JSON.parse(currentPackage), JSON.parse(configPackage))
 
   await fs.writeFile(pathCurrentPackage, JSON.stringify(mergedPackage, null, 2))
+  
   console.log("✍️ Properties (scripts & congig) added to package.json...")
-
 })
 
 
