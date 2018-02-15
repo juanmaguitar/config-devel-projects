810b381f080475738c6e0f1575c610223aa229b2
JuanMa Garrido <juanma.garrido@gmail.com>
2018-02-15 19:13:06 +0100

    variable url bash

--- eslint-standard-prettier/install.sh
+++ eslint-standard-prettier/install.sh
@@ -2,8 +2,10 @@ npm init -y
 npm i -D babel-preset-es2015 babel-preset-stage-2 babel-preset-react 
 npm i -D babel-eslint eslint-config-standard eslint-plugin-import eslint-plugin-node eslint-plugin-promise eslint-plugin-standard eslint-plugin-react prettier-eslint
 
-curl -fsSL https://rawgit.com/juanmaguitar/config-devel-projects/master/eslint-standard-prettier/.babelrc > .babelrc
-curl -fsSL https://rawgit.com/juanmaguitar/config-devel-projects/master/eslint-standard-prettier/.eslintrc > .eslintrc
+$GITHUB_RAW_URL="https://raw.githubusercontent.com/juanmaguitar/config-devel-projects/master/eslint-standard-prettier/"
+
+curl -fsSL $GITHUB_RAW_URL+".babelrc" > .babelrc
+curl -fsSL $GITHUB_RAW_URL+".eslintrc" > .eslintrc
 
 npm i lodash request request-promise
-curl -fsSL https://rawgit.com/juanmaguitar/config-devel-projects/master/eslint-standard-prettier/mergePackage.js | node
+curl -fsSL $GITHUB_RAW_URL+"mergePackage.js" | node
