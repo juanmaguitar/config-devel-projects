6a1939b273cb65fa951795b4c06f6aa016ba1826
JuanMa Garrido <juanma.garrido@gmail.com>
2018-02-15 19:17:37 +0100

    fixed variable

--- eslint-standard-prettier/install.sh
+++ eslint-standard-prettier/install.sh
@@ -4,8 +4,8 @@ npm i -D babel-eslint eslint-config-standard eslint-plugin-import eslint-plugin-
 
 GITHUB_RAW_URL="https://raw.githubusercontent.com/juanmaguitar/config-devel-projects/master/eslint-standard-prettier/"
 
-curl -fsSL $GITHUB_RAW_URL+".babelrc" > .babelrc
-curl -fsSL $GITHUB_RAW_URL+".eslintrc" > .eslintrc
+curl -fsSL $GITHUB_RAW_URL".babelrc" > .babelrc
+curl -fsSL $GITHUB_RAW_URL".eslintrc" > .eslintrc
 
 npm i lodash request request-promise
-curl -fsSL $GITHUB_RAW_URL+"mergePackage.js" | node
+curl -fsSL $GITHUB_RAW_URL"mergePackage.js" | node
