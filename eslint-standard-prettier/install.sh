0a29ae60412ce53046020113b23fb865ed0cb188
JuanMa Garrido <juanma.garrido@gmail.com>
2018-02-15 20:58:35 +0100

    vars in shell

--- eslint-standard-prettier/install.sh
+++ eslint-standard-prettier/install.sh
@@ -1,9 +1,13 @@
 GITHUB_RAW_URL="https://raw.githubusercontent.com/juanmaguitar/config-devel-projects/master/eslint-standard-prettier/"
 
-curl -fsSL $GITHUB_RAW_URL".babelrc" > .babelrc
-curl -fsSL $GITHUB_RAW_URL".eslintrc" > .eslintrc
+REMOTE_BABEL_FILE=$GITHUB_RAW_URL".babelrc"
+REMOTE_ESLINT_FILE=$GITHUB_RAW_URL".eslintrc"
+REMOTE_MERGE_PACKAGES_NODE=$GITHUB_RAW_URL"mergePackage.js"
+
+curl -fsSL $REMOTE_BABEL_FILE > .babelrc
+curl -fsSL $REMOTE_ESLINT_FILE > .eslintrc
 
 npm i lodash request request-promise
-GITHUB_RAW_URL=$GITHUB_RAW_URL node -e $(curl -fsSL $GITHUB_RAW_URL"mergePackage.js")
+GITHUB_RAW_URL=$GITHUB_RAW_URL node -e "$(curl -fsSL $REMOTE_MERGE_PACKAGES_NODE)"
 cat package.json
 npm i
