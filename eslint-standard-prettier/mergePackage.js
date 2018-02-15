e4efccf4ce9ae04483aa1a145d5ab83fbe2efcd4
JuanMa Garrido <juanma.garrido@gmail.com>
2018-02-15 11:35:56 +0100

    merged another method

--- eslint-standard-prettier/mergePackage.js
+++ eslint-standard-prettier/mergePackage.js
@@ -3,18 +3,20 @@ const rp = require('request-promise')
 
 const urlConfigPackage = 'https://rawgit.com/juanmaguitar/config-devel-projects/master/eslint-standard-prettier/package.json'
 
-(async function() {
+console.log(__dirname)
+console.log(process.cwd)
 
-  console.log(__dirname)
-  console.log(process.cwd)
+rp(urlConfigPackage)
+  .then(configPackage => {
 
-  const configPackage = await rp(urlConfigPackage)
-  const currentPackage = require('package.json')
+    const currentPackage = require('package.json')
 
-  const mergedPackage = Object.assign({}, JSON.parse(configPackage), currentPackage)
-  console.log(mergedPackage)
+    const mergedPackage = Object.assign({}, JSON.parse(configPackage), currentPackage)
+    console.log(mergedPackage)
 
-  await fs.writeFile('package.json', JSON.stringify(mergedPackage, null, 2))
-  console.log("✍️ Scripts added to package.json...")
+    fs.writeFileSync('package.json', JSON.stringify(mergedPackage, null, 2))
+    console.log("✍️ Scripts added to package.json...")
 
-})()
+  })
+
+
