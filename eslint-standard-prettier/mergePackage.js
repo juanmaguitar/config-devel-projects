561a5eadf1c539acceb29126e6bfba645aea4194
JuanMa Garrido <juanma.garrido@gmail.com>
2018-02-15 19:26:43 +0100

    fixed variable

--- eslint-standard-prettier/mergePackage.js
+++ eslint-standard-prettier/mergePackage.js
@@ -11,12 +11,12 @@ console.log(pathCurrentPackage)
 
 (async function () {
 
-  const configPackage = async rp(urlConfigPackage)
+  const configPackage = await rp(urlConfigPackage)
   
   const currentPackage = fs.readFileSync(pathCurrentPackage, 'utf-8')
   const mergedPackage = _.merge(JSON.parse(currentPackage), JSON.parse(configPackage))
 
-  async fs.writeFile(pathCurrentPackage, JSON.stringify(mergedPackage, null, 2))
+  await fs.writeFile(pathCurrentPackage, JSON.stringify(mergedPackage, null, 2))
   console.log("✍️ Properties (scripts & congig) added to package.json...")
 
 })
