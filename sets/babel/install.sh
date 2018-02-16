a6f50bbb045c8bcb183c2b06bff9af873ae4191e
JuanMa Garrido <juanma.garrido@gmail.com>
2018-02-16 20:18:34 +0100

    paths fixed

--- sets/babel/install.sh
+++ sets/babel/install.sh
@@ -1,7 +1,7 @@
 GITHUB_SET_RAW_URL="https://raw.githubusercontent.com/juanmaguitar/config-devel-projects/master/sets/eslint-standard-prettier/"
 GITHUB_HELPERS_RAW_URL="https://raw.githubusercontent.com/juanmaguitar/config-devel-projects/master/helpers/"
 
-REMOTE_BABEL_FILE=$GITHUB_RAW_URL".babelrc"
+REMOTE_BABEL_FILE=$GITHUB_SET_RAW_URL".babelrc"
 REMOTE_MERGE_PACKAGES_NODE=$GITHUB_HELPERS_RAW_URL"mergePackage.js"
 
 echo "Creating .babelrc"
@@ -9,6 +9,6 @@ curl -fsSL $REMOTE_BABEL_FILE > .babelrc
 
 echo "Merging properties in package.json ..."
 npm i lodash request request-promise
-GITHUB_RAW_URL=$GITHUB_RAW_URL node -e "$(curl -fsSL $REMOTE_MERGE_PACKAGES_NODE)"
+GITHUB_RAW_URL=$GITHUB_SET_RAW_URL node -e "$(curl -fsSL $REMOTE_MERGE_PACKAGES_NODE)"
 cat package.json
 npm i
--- sets/webpack/install.sh
+++ sets/webpack/install.sh
@@ -1,15 +1,15 @@
-GITHUB_RAW_URL="https://raw.githubusercontent.com/juanmaguitar/config-devel-projects/master/sets/webpack/"
+GITHUB_SET_RAW_URL="https://raw.githubusercontent.com/juanmaguitar/config-devel-projects/master/sets/webpack/"
 GITHUB_HELPERS_RAW_URL="https://raw.githubusercontent.com/juanmaguitar/config-devel-projects/master/helpers/"
 
-REMOTE_WEBPACK=$GITHUB_RAW_URL"webpack.config.js"
+REMOTE_WEBPACK=$GITHUB_SET_RAW_URL"webpack.config.js"
 
-REMOTE_COMMON_PATHS=$GITHUB_RAW_URL"build-utils/common-paths.js"
-REMOTE_WEBPACK_COMMON=$GITHUB_RAW_URL"build-utils/webpack.common.js"
-REMOTE_WEBPACK_DEV=$GITHUB_RAW_URL"build-utils/webpack.dev.js"
-REMOTE_WEBPACK_PROD=$GITHUB_RAW_URL"build-utils/webpack.prod.js"
+REMOTE_COMMON_PATHS=$GITHUB_SET_RAW_URL"build-utils/common-paths.js"
+REMOTE_WEBPACK_COMMON=$GITHUB_SET_RAW_URL"build-utils/webpack.common.js"
+REMOTE_WEBPACK_DEV=$GITHUB_SET_RAW_URL"build-utils/webpack.dev.js"
+REMOTE_WEBPACK_PROD=$GITHUB_SET_RAW_URL"build-utils/webpack.prod.js"
 
-REMOTE_WEBPACK_ADDON_BUNDLEANALYZE=$GITHUB_RAW_URL"addons/webpack.bundleanalyze.js"
-REMOTE_WEBPACK_ADDON_BUNDLEBUDDY=$GITHUB_RAW_URL"addons/webpack.bundlebuddy.js"
+REMOTE_WEBPACK_ADDON_BUNDLEANALYZE=$GITHUB_SET_RAW_URL"addons/webpack.bundleanalyze.js"
+REMOTE_WEBPACK_ADDON_BUNDLEBUDDY=$GITHUB_SET_RAW_URL"addons/webpack.bundlebuddy.js"
 
 echo "Creating build-utils folder & files ..."
 mkdir build-utils
@@ -24,6 +24,6 @@ REMOTE_MERGE_PACKAGES_NODE=$GITHUB_HELPERS_RAW_URL"mergePackage.js"
 
 echo "Merging properties in package.json ..."
 npm i lodash request request-promise
-GITHUB_RAW_URL=$GITHUB_RAW_URL node -e "$(curl -fsSL $REMOTE_MERGE_PACKAGES_NODE)"
+GITHUB_RAW_URL=$GITHUB_SET_RAW_URL node -e "$(curl -fsSL $REMOTE_MERGE_PACKAGES_NODE)"
 cat package.json
 npm i
