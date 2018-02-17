b5fe723974c339ea2bb0aaaf0782d6c8be4898d1
JuanMa Garrido <juanma.garrido@gmail.com>
2018-02-17 12:25:05 +0100

    fixed path

--- sets/webpack-src/install.sh
+++ sets/webpack-src/install.sh
@@ -6,15 +6,15 @@ echo "Creating build-utils folder & files ..."
 mkdir src
 mkdir src/{assets,js,scss}
 
-REMOTE_FAVICON=$GITHUB_SET_RAW_URL"assets/favicon.ico"
+REMOTE_FAVICON=$GITHUB_SET_RAW_URL"src/assets/favicon.ico"
 curl -fsSL $REMOTE_FAVICON > assets/favicon.ico
 
-REMOTE_INDEX_TEMPLATE=$GITHUB_SET_RAW_URL"assets/index.template.html"
+REMOTE_INDEX_TEMPLATE=$GITHUB_SET_RAW_URL"src/assets/index.template.html"
 curl -fsSL $REMOTE_INDEX_TEMPLATE > assets/index.template.html
 
-REMOTE_INDEX_JS=$GITHUB_SET_RAW_URL"js/index.js"
+REMOTE_INDEX_JS=$GITHUB_SET_RAW_URL"src/js/index.js"
 curl -fsSL $REMOTE_INDEX_JS > js/index.js
 
-REMOTE_INDEX_SASS=$GITHUB_SET_RAW_URL"scss/index.scss"
+REMOTE_INDEX_SASS=$GITHUB_SET_RAW_URL"src/scss/index.scss"
 curl -fsSL $REMOTE_INDEX_SASS > scss/index.scss
 
